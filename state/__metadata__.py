74e05b7c9ea00d793a15323371baaee2fb0160a0
Andrey Kononykhin <andkononykhin@gmail.com>
2017-04-13 17:14:36 +0300

    updated version info to match common approach

--- state/__metadata__.py
+++ state/__metadata__.py
@@ -1,7 +1,7 @@
 """
 patricia tree package metadata
 """
-__version_info__ = (0, 0, 1)
+__version_info__ = (0, 1)
 __version__ = '{}.{}.{}'.format(*__version_info__)
 __author__ = "Evernym, Inc."
 __license__ = "Apache 2.0"
